e890f5824958e45720d5d8955db73906b47f5ef6
Alistair Halpern <halperna@bhf.org.uk>
2025-09-26 12:38:28 +0100

    Create Document library and folders with test permissions

---
+++ Get/CreatePWC_EF_Site.ps1
@@ -0,0 +1,64 @@
+
+Param(
+    [Parameter(Mandatory = $true )][string] $SiteUrl
+)
+
+$env:ENTRAID_APP_ID = '39a54693-963c-4ebb-aad0-3e2bc2c9a6b4'
+Import-Module Microsoft.PowerShell.Utility;
+Import-Module PnP.PowerShell
+
+	
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+#Import-Module BHF.PnP.Addons
+
+
+If ($null -eq ${Function New-Guid}) {
+    Function New-Guid {
+        return [guid]::NewGuid()
+    }
+}
+#
+
+
+
+Function New-WorkdayDataList($ListName, $ListUrl, $Connection) {
+    #$ListName = "Rejections"
+    #$ListUrl = "Documents/Rejections"
+	If($null -ne (Get-PnPList -Identity $ListName -ErrorAction SilentlyContinue)){
+       Remove-PnPList -Force -Identity $ListName -Connection $connection 
+	}
+	New-PnPList -Title $ListName -Url $ListUrl -Template DocumentLibrary
+	Add-PnPFolder -Name 'Input' -Folder $ListName
+	Add-PnPFolder -Name 'Output' -Folder $ListName
+	#Set-PnPGroupPermissions -Identity 'RES-SP-DATA-BHF-CIR-DEV_Test-RO' -List $ListName -AddRole Read
+	#Set-PnPGroupPermissions -Identity 'RES-SP-DATA-BHF-CIR-DEV_test-RW' -List $ListName -AddRole Contribute
+	
+	Set-PnPFolderPermission -List $ListName -Identity $ListName'/Input' -Group 'HROwnersTest' -AddRole 'Contribute' -ClearExisting
+	Set-PnPFolderPermission -List $ListName -Identity $ListName'/Output' -Group 'HROwnersTest' -AddRole 'Read' -ClearExisting
+	}
+
+#Read more: https://www.sharepointdiary.com/2017/03/add-yes-no-check-box-field-to-sharepoint-list-using-powershell.html#ixzz7PfnvrnL0
+
+Try {
+
+    Connect-PnPOnline -Url $SiteUrl -Interactive 
+    $Connection= Get-PnpConnection
+	New-WorkdayDataList -ListName 'Rejections' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'ListerPlanning' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'ListerKPIs' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'Refunds' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'G4SCashandCheques' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'MyVolunteer' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'FlashStockCounts' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'AdobeCommerce' -ListUrl $listUrlWorkdayData -Connection $connection
+	New-WorkdayDataList -ListName 'Budgetsandtargets' -ListUrl $listUrlWorkdayData -Connection $connection
+}
+catch {
+    $e =    $_.Exception
+    $line = $_.InvocationInfo.ScriptLineNumber
+    $msg =  $e.Message 
+    Write-Host -ForegroundColor Red "caught exception: $msg at $line"
+    write-host "Error adding field: $($_.Exception.Message)" -foregroundcolor Red
+    Write-Host "Stack Trace: $($_.ScriptStackTrace)"  -ForegroundColor Red
+    Exit
+}
